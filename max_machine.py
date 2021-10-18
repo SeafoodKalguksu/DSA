3cf39d20d93737af91c2bda4f922607301d86dba
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-10-18 23:24:08 +0900

    + MaxMachine using list

---
+++ max_machine.py
@@ -0,0 +1,31 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+class MaxMachine:
+    def __init__(self) -> None:
+        self.data = []
+
+    def add_number(self, number):
+        self.data.append(number)
+
+    def remove_number(self, number):
+        self.data.remove(number)
+
+    def get_max(self):
+        return max(self.data)
+
+def main():
+    my_machine = MaxMachine()
+    my_machine.add_number(1)
+    my_machine.add_number(2)
+    my_machine.add_number(3)
+    my_machine.add_number(2)
+    print(my_machine.get_max())
+
+    my_machine.remove_number(3)
+    print(my_machine.get_max())
+    my_machine.remove_number(2)
+    print(my_machine.get_max())
+    my_machine.remove_number(2)
+    print(my_machine.get_max())
