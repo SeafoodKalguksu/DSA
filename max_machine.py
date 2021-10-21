dffa0c7bfb9c1281d3aa71439e5907139b15a99f
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-10-22 00:08:15 +0900

    + Function and Variable

--- max_machine.py
+++ max_machine.py
@@ -2,20 +2,22 @@
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 
+from typing import List
+
 class MaxMachine:
     def __init__(self) -> None:
-        self.data = []
+        self.data: List[int] = []
 
-    def add_number(self, number):
+    def add_number(self, number: int) -> None:
         self.data.append(number)
 
-    def remove_number(self, number):
+    def remove_number(self, number: int) -> None:
         self.data.remove(number)
 
-    def get_max(self):
+    def get_max(self) -> int:
         return max(self.data)
 
-def main():
+def main() -> None:
     my_machine = MaxMachine()
     my_machine.add_number(1)
     my_machine.add_number(2)
