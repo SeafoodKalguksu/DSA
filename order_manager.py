0a6fab0eac9d6ee45535cd2e7f906b2fa11295d2
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-10-26 23:47:16 +0900

    fix: make_order -> place_order

--- order_manager.py
+++ order_manager.py
@@ -8,7 +8,7 @@ class OrderSystem:
     def __init__(self) -> None:
         self.data: List[int] = []
 
-    def make_order(self, order_id: int) -> None:
+    def place_order(self, order_id: int) -> None:
         self.data.append(order_id)
 
     def remove_order(self, order_id: int) -> None:
@@ -19,7 +19,7 @@ class OrderSystem:
             return self.data.index(order_id) + 1
         else:
             return -1
-    
+
     def remain_orders(self) -> List[int]:
         return self.data
 
@@ -27,21 +27,20 @@ class OrderSystem:
 def main():
     order_system = OrderSystem()
 
-    order_system.make_order(2)
+    order_system.place_order(2)
     order_system.remove_order(2)
-    order_system.make_order(1818)
-    order_system.make_order(8282)
-    order_system.make_order(2255)
-    order_system.make_order(6515)
+    order_system.place_order(1818)
+    order_system.place_order(8282)
+    order_system.place_order(2255)
+    order_system.place_order(6515)
     order_system.remove_order(1818)
-    order_system.make_order(486)
+    order_system.place_order(486)
 
     print(f'order_system.get_order_status(486) = {order_system.get_order_status(486)}')
     print(f'order_system.get_order_status(3) = {order_system.get_order_status(3)}')
 
-    order_system.make_order(4860)
+    order_system.place_order(4860)
     print(f'remain orders = {order_system.remain_orders()}')
-
 
 
 if __name__ == "__main__": # 1 -1 2
