6df1c29571055843b34cb9c45fe4581a98d368ec
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-10-31 23:45:57 +0900

    fix: 'item: int' -> 'item: Any'

--- stack.py
+++ stack.py
@@ -2,15 +2,13 @@
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 
-from typing import List
-
-MAX_STACK_SIZE = 5
+from typing import List, Any
 
 class Stack:
     def __init__(self, max_size: int) -> None:
-        self.items: List[int] = []
+        self.items: List[Any] = []
         self.max_size = max_size
-        self.top = 0
+        self.top: int = 0
 
     def is_full(self) -> bool:
         return True if self.top == self.max_size else False
@@ -18,23 +16,23 @@ class Stack:
     def is_empty(self) -> bool:
         return True if self.top == 0 else False
 
-    def push(self, item: int) -> None:
+    def push(self, item: Any) -> None:
         if not self.is_full():
             self.items.append(item)
             self.top += 1
         else:
             print("can't push because the stack is full!")
 
-    def pop(self) -> None:
+    def pop(self) -> Any:
         if not self.is_empty():
-            self.items.pop()
             self.top -= 1
+            return self.items.pop()
         else:
             print("can't pop because the stack is empty!")
 
 
 def test_samples() -> None:
-    my_stack = Stack(MAX_STACK_SIZE)
+    my_stack = Stack(5)
     my_stack.push(1)
     my_stack.push(2)
     my_stack.push(3)
