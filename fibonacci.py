9f0beda15bb6829a714c969e05571568bd631fbd
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-12-18 07:09:10 +0900

    + design Fibonacci class + exceptions for a  negative nth number

--- fibonacci.py
+++ fibonacci.py
@@ -4,68 +4,93 @@
 
 from typing import List
 
-# Fibonacci number
-# F0 = 0, F1= 1, F2 = F0 + F1, ... , Fn+2 = Fn + Fn+1
 
+class Fibonacci():
+    '''
+    Fibonacci number
+    F₀ = 0, F₁= 1, F₂ = F₀ + F₁, ... , F𝚗₊₂ = F𝚗 + F𝚗₊₁
+    '''
 
-# Return Nth fibonacci number
-def get_fibonacci_number_using_loop(nth: int) -> int:
-    if nth < 0:
-        print('nth should be greater than -1.')
-        return None
-    elif nth == 0 or nth == 1:
-        return nth
+    def __init__(self) -> None:
+        pass
 
-    fibo_0, fibo_1 = 0, 1
-    start = 2  # start from fibo_2
+    def get_number(self, nth: int) -> int:
+        '''
+        Return Nth fibonacci number
+        nth: 5 -> f₅
+        '''
 
-    # From fibo_2 to fibo_nth
-    while start < nth:
-        fibo_2 = fibo_0 + fibo_1
-        fibo_0, fibo_1 = fibo_1, fibo_2
-        start += 1
+        def using_loop(nth: int) -> int:
+            if nth == 0 or nth == 1:
+                return nth
 
-    return fibo_2
+            fibo_0, fibo_1 = 0, 1
+            start = 2  # start from fibo_2
 
+            # From fibo_2 to fibo_nth
+            while start < nth + 1:
+                fibo_2 = fibo_0 + fibo_1
+                fibo_0, fibo_1 = fibo_1, fibo_2
+                start += 1
 
-# Return a list which has fibonacci numbers from f0 to fn
-def get_fibonacci_numbers_using_loop(length: int) -> List[int]:
-    fibonacci_numbers: List[int] = []
-    fibo_0: int = 0
-    fibo_1: int = 1
-    index: int = 2  # start from fibo_3
+            return fibo_2
 
-    fibonacci_numbers.append(fibo_0)
-    fibonacci_numbers.append(fibo_1)
+        def using_recursion(nth: int) -> int:
+            return nth if nth < 2 else using_recursion(nth - 2) + using_recursion(nth - 1)
 
-    while index < length:
-        fibonacci_numbers.append(fibo_0 + fibo_1)
-        fibo_0, fibo_1 = fibo_1, fibo_0 + fibo_1
-        index += 1
+        try:
+            if nth < 0:
+                raise Exception('f𝚗: nth must be greater than -1.')
+        except Exception as error:
+            print(error)
+            return None
+        else:
+            return using_loop(nth)
+            # return using_recursion(nth)
 
-    return fibonacci_numbers
+    def get_numbers(self, nth: int) -> List[int]:
+        '''
+        Return a list which has fibonacci numbers from f0 to fn
+        nth: 5 -> f₅
+        '''
+        fibonacci_numbers: List[int] = []
 
+        def using_loop(nth: int) -> List[int]:
+            fibo_0: int = 0
+            fibo_1: int = 1
+            index: int = 2  # start from fibo_3
 
-# Return Nth fibonacci number using recursion
-def get_fibonacci_number_using_recursion(nth: int) -> int:
-    return nth if nth < 2 else get_fibonacci_number_using_recursion(nth-2) + get_fibonacci_number_using_recursion(nth - 1)
+            fibonacci_numbers.append(fibo_0)
+            fibonacci_numbers.append(fibo_1)
 
+            while index < nth + 1:
+                fibonacci_numbers.append(fibo_0 + fibo_1)
+                fibo_0, fibo_1 = fibo_1, fibo_0 + fibo_1
+                index += 1
 
-# Return a list which has fibonacci numbers from f0 to fn
-def get_fibonacci_numbers_using_recursion(length: int) -> List[int]:
-    fibonacci_numbers: List[int] = []
+            return fibonacci_numbers
 
-    for i in range(0, length):
-        fibonacci_numbers.append(get_fibonacci_number_using_recursion(i))
+        def using_recursion(nth: int) -> List[int]:
+            for index in range(0, nth + 1):
+                fibonacci_numbers.append(self.get_number(index))
 
-    return fibonacci_numbers
+            return fibonacci_numbers
+
+        try:
+            if nth < 0:
+                raise Exception('f𝚗: nth must be greater than -1.')
+        except Exception as error:
+            print(error)
+            return None
+        else:
+            return using_loop(nth)
+            # return using_recursion(nth)
 
 
 def main():
-    print(get_fibonacci_number_using_loop(10))
-    print(get_fibonacci_numbers_using_loop(10))
-    print(get_fibonacci_number_using_recursion(9))
-    print(get_fibonacci_numbers_using_recursion(10))
+    fibonacci = Fibonacci()
+    print(fibonacci.get_number(10))
+    print(fibonacci.get_numbers(10))
 
 
 if __name__ == "__main__":
