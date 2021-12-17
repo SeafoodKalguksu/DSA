27cff87be1cd9aa26361d9ce7c7a6df08650ed36
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-12-17 23:51:56 +0900

    + get_fibonacci_number_using_recursion()

--- fibonacci.py
+++ fibonacci.py
@@ -8,7 +8,7 @@ from typing import List
 # F0 = 0, F1= 1, F2 = F0 + F1, ... , Fn+2 = Fn + Fn+1
 
 
-# Return Nth fibonacci number.
+# Return Nth fibonacci number
 def get_fibonacci_number_using_loop(nth: int) -> int:
     if nth < 0:
         print('nth should be greater than -1.')
@@ -29,7 +29,7 @@ def get_fibonacci_number_using_loop(nth: int) -> int:
 
 
 # Return a list which has fibonacci numbers from f0 to fn
-def get_fibonacci_numbers(length: int) -> List[int]:
+def get_fibonacci_numbers_using_loop(length: int) -> List[int]:
     fibonacci_numbers: List[int] = []
     fibo_0: int = 0
     fibo_1: int = 1
@@ -46,9 +46,26 @@ def get_fibonacci_numbers(length: int) -> List[int]:
     return fibonacci_numbers
 
 
+# Return Nth fibonacci number using recursion
+def get_fibonacci_number_using_recursion(nth: int) -> int:
+    return nth if nth < 2 else get_fibonacci_number_using_recursion(nth-2) + get_fibonacci_number_using_recursion(nth - 1)
+
+
+# Return a list which has fibonacci numbers from f0 to fn
+def get_fibonacci_numbers_using_recursion(length: int) -> List[int]:
+    fibonacci_numbers: List[int] = []
+
+    for i in range(0, length):
+        fibonacci_numbers.append(get_fibonacci_number_using_recursion(i))
+
+    return fibonacci_numbers
+
+
 def main():
     print(get_fibonacci_number_using_loop(10))
-    print(get_fibonacci_numbers(10))
+    print(get_fibonacci_numbers_using_loop(10))
+    print(get_fibonacci_number_using_recursion(9))
+    print(get_fibonacci_numbers_using_recursion(10))
 
 
 if __name__ == "__main__":
