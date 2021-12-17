9e936f271ed98390d8f9befa2b31589f944dd5ce
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-12-17 23:22:06 +0900

    + get_fibonacci_number_using_loop(), get_fibonacci_numbers()

--- fibonacci.py
+++ fibonacci.py
@@ -2,23 +2,54 @@
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 
+from typing import List
+
 # Fibonacci number
 # F0 = 0, F1= 1, F2 = F0 + F1, ... , Fn+2 = Fn + Fn+1
 
-def fibonacci_with_loop(number: int):
-    if number < 0:
-        print('number should be greater than -1.')
+
+# Return Nth fibonacci number.
+def get_fibonacci_number_using_loop(nth: int) -> int:
+    if nth < 0:
+        print('nth should be greater than -1.')
         return None
-    elif number == 0 or number == 1:
-        return number
+    elif nth == 0 or nth == 1:
+        return nth
 
     fibo_0, fibo_1 = 0, 1
-    start = 2
+    start = 2  # start from fibo_2
 
-    # From fibo_2 to fmax
-    while start < number+1:
+    # From fibo_2 to fibo_nth
+    while start < nth:
         fibo_2 = fibo_0 + fibo_1
         fibo_0, fibo_1 = fibo_1, fibo_2
         start += 1
 
     return fibo_2
+
+
+# Return a list which has fibonacci numbers from f0 to fn
+def get_fibonacci_numbers(length: int) -> List[int]:
+    fibonacci_numbers: List[int] = []
+    fibo_0: int = 0
+    fibo_1: int = 1
+    index: int = 2  # start from fibo_3
+
+    fibonacci_numbers.append(fibo_0)
+    fibonacci_numbers.append(fibo_1)
+
+    while index < length:
+        fibonacci_numbers.append(fibo_0 + fibo_1)
+        fibo_0, fibo_1 = fibo_1, fibo_0 + fibo_1
+        index += 1
+
+    return fibonacci_numbers
+
+
+def main():
+    print(get_fibonacci_number_using_loop(10))
+    print(get_fibonacci_numbers(10))
+
+
+if __name__ == "__main__":
+    main()
