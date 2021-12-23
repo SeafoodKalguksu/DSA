e56d2c8fe446823e0efff57fabcf1424b08b1f4c
SeafoodKalguksu <heeseok.chong@icloud.com>
2021-12-23 23:39:28 +0900

    refactoring: nested functions

--- palindrome.py
+++ palindrome.py
@@ -2,12 +2,17 @@
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 
-def is_palindrome(sentence: str) -> bool:
+
+USING_REVERSE: int = 0
+USING_RECURRSION: int = 1
+
+
+def is_palindrome(sentence: str, select: int = USING_REVERSE) -> bool:
     # Reverse and compare
     def reverse_compare(sentence: str) -> bool:
         return True if sentence == sentence[-1::-1] else False
 
-    # Recursive func.
+    # Recursive func
     def recursive_call(sentence: str) -> bool:
         length: int = len(sentence)
 
@@ -21,4 +26,19 @@ def is_palindrome(sentence: str) -> bool:
         else:
             return False
 
-    return recursive_call(sentence)  # return reverse_compare(sentence)
+    return (
+        recursive_call(sentence)
+        if select == USING_RECURRSION
+        else reverse_compare(sentence)
+    )
+
+
+def main():
+    print(is_palindrome("abcdefedcba", USING_RECURRSION))
+    print(is_palindrome("abcdeffedcba"))
+    print(is_palindrome("abcdefffedcba", USING_RECURRSION))
+    print(is_palindrome("abcdeffefedcba"))
+
+
+if __name__ == "__main__":
+    main()
